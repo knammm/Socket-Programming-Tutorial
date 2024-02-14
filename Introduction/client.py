f711ef1518e7e7ba8d09d5fbf4a0434ffa4ca433
olkmphy <nam.nguyenolkmphy@hcmut.edu.vn>
2024-02-14 17:32:49 +0700

    Upgrade client

--- Introduction/client.py
+++ Introduction/client.py
@@ -6,5 +6,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 12000))
 
 # This is a buffer. How big of chunks of data do we want to receive at a time
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+full_msg = ''
+while True:
+	msg = s.recv(8)
+	if(len(msg) <= 0):
+		break
+	full_msg += msg.decode("utf-8")
+
+print(full_msg)
